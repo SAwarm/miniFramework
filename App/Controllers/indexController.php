eff9e9bf15eb45471c0465fc835ec15f22a470d6
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-11 22:19:28 -0300

    ADD: View Files

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -4,13 +4,33 @@ namespace App\Controllers;
 
 class indexController {
 
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass;
+    }
+
     public function index()
     {
-        echo 'index_controller';
+        $this->view->data = array('text', 'test');
+        $this->render('index');
     }
 
     public function about_us()
     {
-        echo 'about_us_controller';
+        $this->view->data = array('cellphone', 'computer');
+        $this->render('about_us');
+    }
+
+    public function render($view)
+    {
+        $actual_class = get_class($this);
+
+        $actual_class = str_replace('App\\Controllers\\', '', $actual_class);
+
+        $actual_class = strtolower(str_replace('Controller', '', $actual_class));
+
+        require_once "./App/Views/".$actual_class."/".$view.".phtml";
     }
 }
